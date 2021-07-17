ff560109de417bd4d7119ff5455eff366a7e0d2b
justiceqaq <justiceqaq@gmail.com>
2021-07-17 10:49:08 +0800

    feat: 增加影片辨識

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -1,16 +1,22 @@
+import os
 import streamlit as st
 
 from haarcascades_unit import HaarCascades
 
 st.title("互動式圖像人臉辨識")
 
-uploaded_file = st.file_uploader("請上傳圖片")
+uploaded_file = st.file_uploader("請上傳圖片", type=['png', 'jpg'])
 col1, col2 = st.beta_columns(2)
+col3, col4 = st.beta_columns(2)
 
 if uploaded_file is not None:
+    image_load_state = st.text('正在辨識圖片...')
     bytes_data = uploaded_file.getvalue()
     cv2_image = HaarCascades.read_image(bytes_data).to_cv2_haarcascades()
     dlib_image = HaarCascades.read_image(bytes_data).to_dlib_haarcascades()
+
+    cv2_smile_image = HaarCascades.read_image(bytes_data).to_cv2_smile()
+    cv2_eye_image = HaarCascades.read_image(bytes_data).to_cv2_eye()
 
     with col1:
         st.header(
@@ -24,3 +30,57 @@ if uploaded_file is not None:
         predictor 68 face landmarks
         """)
         st.image(dlib_image)
+    with col3:
+        st.header(
+            """Used CV2
+        haarcascade smile
+        """)
+        st.image(cv2_smile_image)
+    with col4:
+        st.header(
+            """Used CV2
+        haarcascade eye
+        """)
+        st.image(cv2_eye_image)
+
+    image_load_state.text("")
+    # st.balloons()
+
+uploaded_video_file = st.file_uploader("請上傳影片", type=['mp4'])
+data_load_state = st.text('')
+col5, col6 = st.beta_columns(2)
+if uploaded_video_file is not None:
+    data_load_state = st.text('正在辨識影片...')
+    if os.path.exists("cv2_output.mp4"):
+        os.remove("cv2_output.mp4")
+        col5_video = st.empty()
+    if os.path.exists("dlib_output.mp4"):
+        os.remove("dlib_output.mp4")
+        col5_video = st.empty()
+
+    video_bytes = uploaded_video_file.read()
+    haarcascades_check = HaarCascades.read_video(video_bytes).used_cv2_haarcascades()
+    dlib_check = HaarCascades.read_video(video_bytes).used_dlib_haarcascades()
+
+    if haarcascades_check:
+        with col5:
+            video_file = open('cv2_output.mp4', 'rb')
+            cv2_output_data = video_file.read()
+            col5_video = st.video(cv2_output_data)
+    if dlib_check:
+        with col6:
+            dlib_video_file = open('dlib_output.mp4', 'rb')
+            col6_video = st.video(dlib_video_file)
+    data_load_state.text('')
+    st.balloons()
+else:
+    if os.path.exists("cv2_output.mp4"):
+        with col5:
+            video_file = open('cv2_output.mp4', 'rb')
+            cv2_output_data = video_file.read()
+            st.video(cv2_output_data)
+    if os.path.exists("dlib_output.mp4"):
+        with col6:
+            video_file = open('dlib_output.mp4', 'rb')
+            st.video(video_file)
+    data_load_state.text('')
--- haarcascades_unit.py
+++ haarcascades_unit.py
@@ -1,4 +1,6 @@
 import io
+import os
+import tempfile
 
 import dlib
 import numpy as np
@@ -9,6 +11,8 @@ class HaarCascades:
 
     def __init__(self, binary_file=None):
         self.CV2_HAARCASCADE_FRONTALFACE_DEFAULT = "./model/haarcascade_frontalface_default.xml"
+        self.CV2_HAARCASCADE_SMILE = "./model/haarcascade_smile.xml"
+        self.CV2_HAARCASCADE_EYE = "./model/haarcascade_eye.xml"
         self.DLIB_SHAPE_PREDICTOR_68_FACE_LANDMARKS = "./model/shape_predictor_68_face_landmarks.dat"
         self.USED_COLOR = (0, 255, 0)
         self.LINE_WIDE = 4
@@ -41,6 +45,39 @@ class HaarCascades:
                 cv2.rectangle(self.binary_file, (x, y), (x + h, y + w), self.USED_COLOR, self.LINE_WIDE)
         return self.binary_file
 
+    def to_cv2_smile(self):
+        classifier = cv2.CascadeClassifier(self.CV2_HAARCASCADE_FRONTALFACE_DEFAULT)
+        smile_Cascade = cv2.CascadeClassifier(self.CV2_HAARCASCADE_SMILE)
+        face_rects = classifier.detectMultiScale(
+            self.binary_file, scaleFactor=1.2, minNeighbors=4, minSize=(24, 24))
+        if len(face_rects):
+            for faceRect in face_rects:
+                x, y, w, h = faceRect
+                # cv2.rectangle(self.binary_file, (x, y), (x + h, y + w), self.USED_COLOR, self.LINE_WIDE)
+                roi_gray = self.binary_file[y:y + h, x:x + w]
+                roi_color = self.binary_file[y:y + h, x:x + w]
+                smile = smile_Cascade.detectMultiScale(
+                    roi_gray,
+                    scaleFactor=1.16,
+                    minNeighbors=35,
+                    minSize=(25, 25),
+                    flags=cv2.CASCADE_SCALE_IMAGE
+                )
+                for (x2, y2, w2, h2) in smile:
+                    cv2.rectangle(roi_color, (x2, y2), (x2 + w2, y2 + h2), (0, 255, 0), self.LINE_WIDE)
+
+        return self.binary_file
+
+    def to_cv2_eye(self):
+        classifier = cv2.CascadeClassifier(self.CV2_HAARCASCADE_EYE)
+        face_rects = classifier.detectMultiScale(
+            self.binary_file, scaleFactor=1.2, minNeighbors=4, minSize=(24, 24))
+        if len(face_rects):
+            for faceRect in face_rects:
+                x, y, w, h = faceRect
+                cv2.rectangle(self.binary_file, (x, y), (x + h, y + w), self.USED_COLOR, self.LINE_WIDE)
+        return self.binary_file
+
     def to_dlib_haarcascades(self):
         detector = dlib.get_frontal_face_detector()
         predictor = dlib.shape_predictor(
@@ -54,3 +91,70 @@ class HaarCascades:
                 pt_pos = (pt.x, pt.y)
                 cv2.circle(self.binary_file, pt_pos, 4, self.USED_COLOR, self.LINE_WIDE)
         return self.binary_file
+
+    @classmethod
+    def read_video(cls, binary_file):
+        t_file = tempfile.NamedTemporaryFile(delete=False)
+        t_file.write(binary_file)
+        vf = cv2.VideoCapture(t_file.name)
+        class_method = cls(vf)
+        t_file.close()
+        return class_method
+
+    def used_cv2_haarcascades(self):
+        cascade = cv2.CascadeClassifier(self.CV2_HAARCASCADE_FRONTALFACE_DEFAULT)
+        fourcc = cv2.VideoWriter_fourcc(*'H264')
+        out = cv2.VideoWriter('cv2_output.mp4', fourcc, 15.0, (640, 360))
+        flag = 0
+        while True:
+            # Capture frame-by-frame
+            ret, img = self.binary_file.read()
+            if ret == True:
+                # converting to gray image for faster video processing
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                rects = cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4,
+                                                 minSize=(50, 50))
+                # if at least 1 face detected
+                if len(rects) >= 0:
+                    # Draw a rectangle around the faces
+                    for (x, y, w, h) in rects:
+                        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                if flag % 5 == 0:
+                    out.write(img)
+                    flag += 1
+                else:
+                    flag += 1
+            else:
+                break
+        out.release()
+        return True
+
+    def used_dlib_haarcascades(self):
+        detector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor(
+            self.DLIB_SHAPE_PREDICTOR_68_FACE_LANDMARKS
+        )
+        fourcc = cv2.VideoWriter_fourcc(*'H264')
+        out = cv2.VideoWriter('dlib_output.mp4', fourcc, 15.0, (640, 360))
+        flag = 0
+        while True:
+            # Capture frame-by-frame
+            ret, img = self.binary_file.read()
+            if ret:
+                # converting to gray image for faster video processing
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                dets = detector(gray, 1)
+                if flag % 25 == 0:
+                    for face in dets:
+                        shape = predictor(img, face)
+                        for pt in shape.parts():
+                            pt_pos = (pt.x, pt.y)
+                            cv2.circle(img, pt_pos, 3, self.USED_COLOR, 1)
+                    out.write(img)
+                    flag += 1
+                else:
+                    flag += 1
+            else:
+                break
+        out.release()
+        return True
